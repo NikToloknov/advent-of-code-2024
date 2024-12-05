acdb8cdc9400d920ac5b323c18078fc5d1f768ed
nik1yl <nikita.toloknov1@gmail.com>
2024-12-05 23:15:05 +0100

    feat: Add 2nd part to 1st task

--- 01/main.py
+++ 01/main.py
@@ -1,7 +1,7 @@
 from heapq import heappop, heappush
 
 from typing import Iterator
-from collections import Counter
+from collections import defaultdict
 
 
 def read_file(path: str = "input.txt") -> Iterator[tuple[int, int]]:
@@ -20,16 +20,25 @@ def find_distance(left: list[int], right: list[int]) -> int:
     return distance
 
 
+def get_similarity_score(pairs: dict[int, list[int]]) -> int:
+    return sum(left * right for _, (left, right) in pairs.items())
+
+
 def main(path: str):
     lheap: list[int] = []
     rheap: list[int] = []
+    score: dict[int, list[int]] = defaultdict(lambda: [0, 0])
 
     for left, right in read_file(path):
+        score[left][0] += 1
+        score[right][1] += right
+
         heappush(lheap, left)
         heappush(rheap, right)
 
     distance = find_distance(lheap, rheap)
-    print(f"Distance: {distance}")
+    similarity = get_similarity_score(score)
+    print(f"Distance: {distance}, Similarity score: {similarity}")
 
 
 if __name__ == "__main__":
